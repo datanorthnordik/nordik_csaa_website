f6738de6144162f5d89ae5fee30d724b9b6d5399
Athul Narayanan <narayanan@algomau.ca>
2026-05-27 16:59:14 -0400

    CTA changes integrated

--- src/api/pagesApi.ts
+++ src/api/pagesApi.ts
@@ -36,12 +36,20 @@ export type PageQuoteSection = {
   attribution: string
 }
 
+export type PageSectionAsset = {
+  file_url: string
+  fetch_url: string
+  storage_uri: string
+  gcp_object_key: string
+}
+
 export type PageCtaBannerSection = {
   banner_heading: string
   banner_message: string
   button_text: string
   button_url: string
   open_in_new_tab: boolean
+  image?: PageSectionAsset | null
 }
 
 export type PageDocument = {
